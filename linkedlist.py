0b8554a9f5bb6d909c8509fcb4192ab8c3467781
rahulroxx <rahulyupz@gmail.com>
2016-10-22 17:41:58 +0530

    update linkedlist

--- linkedlist.py
+++ linkedlist.py
@@ -1,34 +1,65 @@
-class Node:
-	def __init__(self, data, next=None):
-		self.data = data
-		self.next = next
+class Node(object):
+
+    def __init__(self, data, next=None):
+
+        self.data = data
+        self.next = next
+
+    def __str__(self):
+        return str(self.data)
 
 
-class LinkedList:
-	def __init__(self, head=None):
-		self.head = head
 
-	def add_item(self, data):
-		if self.head is None:
-			n = Node(data)
-			self.head = n
-		else:
-			temp = self.head
-			while temp.next != None:
-				temp = temp.next
-			n = Node(data)
-			temp.next = n
+class LinkedList(object):
 
-	def printList(self):
-		n = self.head 
-		while n:
-			print n.data
-			n = n.next 
+
+
+    def __init__(self):
+
+        self.head = None
+        self.size = 0
+
+
+    def add_elem(self, data):
+            if not self.head:
+                n = Node(data)
+                self.head = n
+                return
+            else:
+                n = self.head
+
+                while n.next != None:
+                    n = n.next
+
+                new_node = Node(data)
+                n.next = new_node;
+                return
+
+
+    def isEmpty(self):
+        return not self.head
+
+    def add_list(self, list_temp):
+        n = self.head
+        while n.next != None:
+            n = n.next
+        n.next = list_temp.head
+
+    def printList(self):
+        n = self.head
+
+        while n:
+            print str(n)
+            n = n.next
 
 ll = LinkedList()
+l2 = LinkedList()
 elems = [1, 2, 3, 54, 6]
-print elems
+elems1 = [33,65,31,5]
 for elem in elems:
-    ll.add_item(elem)
+    ll.add_elem(elem)
+for elem in elems1:
+    l2.add_elem(elem)
 
+ll.add_list(l2)
 ll.printList()
